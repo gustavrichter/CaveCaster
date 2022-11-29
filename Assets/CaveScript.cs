a1bfc8a67862ba96d7d59b6a3e127eef2b21d81e
gustavrichter <104318873+gustavrichter@users.noreply.github.com>
2022-11-29 18:23:03 +0100

    added Event system for combat
    E drücken um gegner spawnen zu lassen,
    der rest geht dann von selbst

--- Assets/CaveScript.cs
+++ Assets/CaveScript.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CaveScript : MonoBehaviour
 {
     public GameObject enemy;
-    public Transform spawnPoint;
+    public Transform[] spawnPoints; //size 3
     private GameObject clonedEnemy;
+    [SerializeField] private GameObject[] m_Enemies; //size 6
+    private List<EnemyScript> m_EnemyScripts;
+    private List<GameObject> m_EnemiesOnFloor;
+    private int m_numberOfEnemies;
+    public Action StageComplete = delegate { };
+    public Action EnemiesAhead = delegate { };
     // Start is called before the first frame update
+    private void Awake()
+    {
+        m_EnemiesOnFloor = new List<GameObject>();
+        m_EnemyScripts = new List<EnemyScript>();
+    }
     void Start()
     {
         
@@ -21,8 +33,37 @@ public class CaveScript : MonoBehaviour
 
     public void SpawnEnemy()
     {
-        clonedEnemy = Instantiate(enemy, this.gameObject.transform) as GameObject;
-        //clonedEnemy.gameObject.transform.SetParent(this.transform, true);
-        
+        m_numberOfEnemies = UnityEngine.Random.Range(1, 4);
+        for (int i = 0; i < m_numberOfEnemies; i++)
+        {
+            m_EnemiesOnFloor.Add(Instantiate(m_Enemies[UnityEngine.Random.Range(1, m_Enemies.Length)]) as GameObject);
+            m_EnemiesOnFloor[i].transform.SetParent(spawnPoints[i].transform, false);
+            m_EnemyScripts.Add(m_EnemiesOnFloor[i].GetComponent<EnemyScript>());
+            m_EnemyScripts[i].m_id = i;
+            m_EnemyScripts[i].EnemyDeath += RemoveEnemy;
+
+        }
+        EnemiesAhead();
+    }
+    private void RemoveEnemy(int id)
+    {
+        m_EnemyScripts[id].EnemyDeath -= RemoveEnemy;
+        m_EnemiesOnFloor[id].gameObject.SetActive(false);
+        m_numberOfEnemies--;
+        if(m_numberOfEnemies <= 0)
+        {
+            FinishStage();
+        }
+    }
+
+    private void FinishStage()
+    {
+        for (int i = 0; i < m_EnemiesOnFloor.Count; i++)
+        {
+            Destroy(m_EnemiesOnFloor[i]);
+        }
+        m_EnemyScripts.Clear();
+        m_EnemiesOnFloor.Clear();
+        StageComplete();
     }
 }
