a92b71ee3cadda99835c97bf87dbbc6dad449ac1
Noxwer-beep <maxi.jatzkowski@gmail.com>
2022-12-29 11:11:29 +0100

    Cave w/ Spawnpoints

---
+++ Assets/SpawnpointScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+bool isCamera 0;
+
+public class SpawnpointScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+        
+    }
+}
