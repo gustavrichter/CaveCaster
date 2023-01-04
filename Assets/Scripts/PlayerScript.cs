df2c8ee075355016b3c85b1ea4f532ba79631d26
gustavrichter <104318873+gustavrichter@users.noreply.github.com>
2023-01-04 18:50:28 +0100

    Added bloody screen

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,6 +10,11 @@ public class PlayerScript : MonoBehaviour
     private MagicBookScript m_MagicBookScript;
     public GameObject m_Cave;
     private CaveScript m_caveScript;
+    [SerializeField]
+    private UnityEngine.UI.Slider m_healthSlider;
+    [SerializeField]
+    private UnityEngine.UI.Image m_hurtScreen;
+
 
 
     private void Awake()
@@ -19,6 +24,7 @@ public class PlayerScript : MonoBehaviour
         //m_Cave = Instantiate(m_Cave.gameObject);
         m_MagicBook = GameObject.Find("MagicBook");
         m_Cave = GameObject.Find("Cave");
+        
     }
    
     void Start()
@@ -28,28 +34,31 @@ public class PlayerScript : MonoBehaviour
         m_caveScript = m_Cave.GetComponent<CaveScript>();
         m_caveScript.EnemiesAhead += OpenBook;
         m_caveScript.StageComplete += CloseBook;
+        m_hurtScreen.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            m_caveScript.SpawnEnemy();
-
-        }
-    }
+ 
     public void TakeDamage(float damage, int element)
     {
         //AkSoundEngine.PostEvent("Combat_Player_damage", gameObject);
         m_fhealth -= damage;
         //Debug.Log(gameObject.name + ": Ouchie! I have " + m_fhealth + " healt left.");
-        //Debug.Log( m_fhealth + " health left.");
-
+        Debug.Log( m_fhealth + " health left.");
+        m_hurtScreen.gameObject.SetActive(true);
+        m_hurtScreen.CrossFadeAlpha(0.0f, .2f, false);
+        StartCoroutine(ShowHurtScreen());
+        m_healthSlider.value = m_fhealth/100.0f;
         if(m_fhealth<= 0)
         {
             //Debug.Log("Game Over.");
         }
+    }
+
+    IEnumerator ShowHurtScreen()
+    {
+        yield return new WaitForSeconds(.2f);
+        m_hurtScreen.CrossFadeAlpha(.2f, .0f, false);
+        m_hurtScreen.gameObject.SetActive(false);
     }
 
     private void OpenBook()
